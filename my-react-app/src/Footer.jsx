6ef58693fb11a13dd7a02c94a5acab0f29b8818d
vivksingh <vivksing655@gmail.com>
2024-03-05 14:49:50 +0530

    Basics of Components

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+function Footer(){
+    
+    return(
+        <footer>
+            <p>&copy; Your Website Name {new Date().getFullYear()}</p>
+        </footer>
+    );
+}
+
+export default Footer
