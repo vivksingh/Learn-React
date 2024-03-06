f83f33030c78efe4dceb2937f7f0958d0a4936db
vivksingh <vivksing655@gmail.com>
2024-03-06 23:08:25 +0530

    created card components

---
+++ Card Component/src/card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+    return(
+        <div className = "card">
+            <img className="card-image" src="https://www.shutterstock.com/image-vector/url-shortener-man-pushes-address-260nw-2201694049.jpg" alt="Profile picture" />
+            <img src={"your profile pic"} alt="my pic" width="200px"/>
+            <h2 className="card-title">&nbsp;&nbsp;Vivek</h2>
+            <p className="card-text">&nbsp;&nbsp;&nbsp;I study react and play video games</p>
+        </div>
+    );
+}
+
+export default Card
